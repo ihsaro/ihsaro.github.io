3797bf45d28d34391a276b6621b45f20191ecec2
Idjaz Hossanee <idjazhossanee@gmail.com>
2021-03-27 15:31:44 +0400

    progress bar

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,21 +3,45 @@ import { Box, Grid, Typography, LinearProgress, Paper, makeStyles } from "@mater
 const useStyles = makeStyles({
     cardSectionOnlyMargin: {
         margin: "20px"
+    },
+    progressBarLabelGrid: {
+        fontFamily: "'Montserrat', sans-serif",
+        marginBottom: "10px"
     }
 })
 
 function LinearProgressWithLabel(props) {
+
+    const classes = useStyles();
+
     return (
-      <Box display="flex" alignItems="center">
-        <Box width="100%" mr={1}>
-          <LinearProgress variant="determinate" {...props} />
-        </Box>
-        <Box minWidth={35}>
-          <Typography variant="body2" color="textSecondary">{`${Math.round(
-            props.value,
-          )}%`}</Typography>
-        </Box>
-      </Box>
+        <Grid 
+            container
+            direction="column"
+        >
+            <Grid
+                container
+                direction="row"
+                justify="space-between"
+                className={classes.progressBarLabelGrid}
+            >
+                <Grid
+                    item
+                >
+                    {props.skillName}
+                </Grid>
+                <Grid
+                    item
+                >
+                    {`${props.value}%`}
+                </Grid>
+            </Grid>
+            <Grid
+                item
+            >
+                <LinearProgress variant="determinate" {...props} />
+            </Grid>
+        </Grid>
     );
 }
 
@@ -30,24 +54,67 @@ export default function Skill() {
             <Grid
                 container
                 direction="row"
+                spacing={4}
             >
                 <Grid
                     item
                     xs={6}
                 >
-                    <LinearProgressWithLabel value={20} />
+                    <LinearProgressWithLabel skillName="HTML" value={65} />
                 </Grid>
                 <Grid
                     item
                     xs={6}
                 >
-                    <LinearProgressWithLabel value={20} />
+                    <LinearProgressWithLabel skillName="CSS" value={50} />
                 </Grid>
                 <Grid
                     item
                     xs={6}
                 >
-                    <LinearProgressWithLabel value={20} />
+                    <LinearProgressWithLabel skillName="Javascript" value={70} />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                >
+                    <LinearProgressWithLabel skillName="Java" value={80} />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                >
+                    <LinearProgressWithLabel skillName="C#" value={80} />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                >
+                    <LinearProgressWithLabel skillName="SQL" value={65} />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                >
+                    <LinearProgressWithLabel skillName="Git" value={50} />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                >
+                    <LinearProgressWithLabel skillName="Linux" value={60} />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                >
+                    <LinearProgressWithLabel skillName="Typescript" value={60} />
+                </Grid>
+                <Grid
+                    item
+                    xs={6}
+                >
+                    <LinearProgressWithLabel skillName="React JS" value={50} />
                 </Grid>
             </Grid>
         </Paper>
